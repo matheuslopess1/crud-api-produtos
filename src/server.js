a5c1b0165312c17fca2d496b0d8e7ee6ef2899b3
Matheus I Lopes <matheuslopes1@protonmail.com>
2020-06-02 08:25:19 -0300

    criando model de produto

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
@@ -8,7 +9,17 @@ mongoose.connect("mongodb://192.168.99.100:27017/nodeapi", {
   useUnifiedTopology: true,
 });
 
+requireDir("./app/models");
+
+const Product = mongoose.model("Product");
+
 app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native",
+  });
+
   res.send("Hello Rocketseat");
 });
 
