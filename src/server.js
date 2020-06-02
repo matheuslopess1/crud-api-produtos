7803bc9174fd566942a4f596c3cd6cfa44da719f
Matheus I Lopes <matheuslopes1@protonmail.com>
2020-06-02 08:18:18 -0300

    conectando database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://192.168.99.100:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Rocketseat");
